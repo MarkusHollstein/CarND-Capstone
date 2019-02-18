413d8a14af455724ebd5526b6d90ff26b4bcf9f5
Markus Hollstein <mhollstein1@gmx.de>
2019-02-18 09:30:26 -0800

    changed order in init

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -24,6 +24,7 @@ class TLDetector(object):
         self.camera_image = None
         self.lights = []
         self.kdTree = None
+	self.count = 0
 
         sub1 = rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb)
         sub2 = rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb)
@@ -51,7 +52,7 @@ class TLDetector(object):
         self.state = TrafficLight.UNKNOWN
         self.last_state = TrafficLight.UNKNOWN
         self.last_wp = -1
-        self.count = 0
+        
 	self.process_nth_image = 3
         self.state_count = 0
 
